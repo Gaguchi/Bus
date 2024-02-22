a084bed5e62255d79102849ca1645419a5ab4915
Gaguchi <gaga9393@gmail.com>
2024-02-22 22:28:29 +0400

    changes

--- busbackend/bus/admin.py
+++ busbackend/bus/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Ticket, Company, City, Route, Transport, Trip
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Company)
+admin.site.register(City)
+admin.site.register(Route)
+admin.site.register(Transport)
+admin.site.register(Trip)
--- busbackend/bus/models.py
+++ busbackend/bus/models.py
@@ -6,13 +6,16 @@ class User(models.Model):
     email = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    trips = models.ManyToManyField('Trip', through='Ticket', related_name='users')
 
 class Ticket(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
-    trip = models.ForeignKey('Trip', on_delete=models.CASCADE, related_name='tickets')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+    routes = models.ManyToManyField('Route', related_name='companies')
+    trips = models.ManyToManyField('Trip', related_name='companies')
 
 class City(models.Model):
     name = models.CharField(max_length=255)
@@ -21,7 +24,6 @@ class City(models.Model):
 class Route(models.Model):
     departures = models.ForeignKey(City, on_delete=models.CASCADE, related_name='departure_routes')
     arrivals = models.ForeignKey(City, on_delete=models.CASCADE, related_name='arrival_routes')
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='routes')
 
 class Transport(models.Model):
     type = models.CharField(max_length=255)
@@ -30,13 +32,4 @@ class Transport(models.Model):
 class Trip(models.Model):
     departure = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='departure_trips')
     arrival = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='arrival_trips')
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='trips')
     transport = models.ForeignKey(Transport, on_delete=models.CASCADE, related_name='trips')
-
-class UserTicket(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-
-class CompanyTrip(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)

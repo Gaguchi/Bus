abc75a381f317a3c642d6db7be5d14ad94777474
Gaguchi <gaga9393@gmail.com>
2024-02-26 23:11:37 +0400

    google login templates (not implemented)

--- busbackend/bus/urls.py
+++ busbackend/bus/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register_api/', views.register_api, name='register_api'),
-    path('login/', views.login_view, name='login'),
+    # path('register_api/', views.register_api, name='register_api'),
+    # path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
 ]
--- busbackend/bus/views.py
+++ busbackend/bus/views.py
@@ -5,26 +5,26 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-@csrf_exempt
-def register_api(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({'error': 'Username already exists.'}, status=400)
-        user = User.objects.create_user(username, password=password)
-        return JsonResponse({'message': 'User created successfully.'}, status=201)
+# @csrf_exempt
+# def register_api(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         if User.objects.filter(username=username).exists():
+#             return JsonResponse({'error': 'Username already exists.'}, status=400)
+#         user = User.objects.create_user(username, password=password)
+#         return JsonResponse({'message': 'User created successfully.'}, status=201)
 
-@csrf_exempt
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            return JsonResponse({'error': 'Invalid username or password.'}, status=400)
-        login(request, user)
-        return JsonResponse({'message': 'Logged in successfully.'}, status=200)
+# @csrf_exempt
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is None:
+#             return JsonResponse({'error': 'Invalid username or password.'}, status=400)
+#         login(request, user)
+#         return JsonResponse({'message': 'Logged in successfully.'}, status=200)
     
 def register(request):
     return render(request, 'bus/signup.html')
